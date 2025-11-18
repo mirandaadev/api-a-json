56a64ca8d1192785c02ac21aa699b89311ea6585
mirandaadev <mirandadiego864@gmail.com>
2025-11-17 20:36:30 -0600

    Implementar consumo de API externa para obtener datos de personajes

--- src/main/java/mx/unam/api/consumo/principal/Principal.java
+++ src/main/java/mx/unam/api/consumo/principal/Principal.java
@@ -1,7 +1,14 @@
 package mx.unam.api.consumo.principal;
 
+import mx.unam.api.consumo.servicio.ConsumoApi;
+import mx.unam.api.consumo.utilidades.ConversorGson;
+
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("ConsumoApi");
+        String url = "https://thesimpsonsapi.com/api/characters";
+        ConsumoApi consumoApi = new ConsumoApi();
+        ConversorGson conversor = new ConversorGson();
+        System.out.println(conversor.formatoJson(consumoApi.obtenerDatos(url)));
     }
 }

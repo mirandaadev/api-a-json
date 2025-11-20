403b9cbe10a1e8dbf5fbc4cc61c704dd984c6502
mirandaadev <mirandadiego864@gmail.com>
2025-11-20 00:51:14 -0600

    Reorganizar código

--- src/main/java/mx/unam/api/consumo/servicio/ConsumoApi.java
+++ src/main/java/mx/unam/api/consumo/servicio/ConsumoApi.java
@@ -9,11 +9,13 @@ import java.net.http.HttpResponse;
 public class ConsumoApi {
     public String obtenerDatos(String url) {
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
         HttpResponse<String> response;
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | RuntimeException | InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
         return response.body();
